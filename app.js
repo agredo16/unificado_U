ccd22bcdcc8c39272108157c58360175265ddfe0
Yudith Agredo <agredocatalina90@gmail.com>
2025-03-06 15:00:38 -0500

    combios en el app

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(compression());
-app.use(timeout('10s'));
+app.use(timeout('30s'));
 app.use(cors());
 
 const limiter = rateLimit({
